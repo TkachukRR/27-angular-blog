b238779a18c8fc1871d00ec8f3ffba756388c3f0
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-07 14:47:02 +0100

    #004 Added route path for CreatePageComponent

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -14,6 +14,7 @@ import { CreatePageComponent } from './create-page/create-page.component';
           {path: '', redirectTo: '/admin/login', pathMatch: "full"},
           {path: 'login', component: LoginPageComponent},
           {path: 'dashboard', component: DashboardPageComponent},
+          {path: 'create', component: CreatePageComponent},
         ]}
     ])
   ],
