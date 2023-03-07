f27d289e99c6d18c1a9d9bed85ff29d97efff4e1
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-07 11:37:27 +0100

    #003 Generated post-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { PostPageComponent } from './post-page/post-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainLayoutComponent,
-    HomePageComponent
+    HomePageComponent,
+    PostPageComponent
   ],
   imports: [
     BrowserModule,
