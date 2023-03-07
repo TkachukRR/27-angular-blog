5a5b3ef20ae50362e8362a3bc5a053c410e80bec
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-07 11:32:21 +0100

    #002 Added children rout to HomePageComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainLayoutComponent} from "./shared/components/main-layout/main-layout.component";
+import {HomePageComponent} from "./home-page/home-page.component";
 
 const routes: Routes = [
   {
     path: '', component: MainLayoutComponent, children: [
-
+      {path: '', component: HomePageComponent}
     ]
   }
 ];
