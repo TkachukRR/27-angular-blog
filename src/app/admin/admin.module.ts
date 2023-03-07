f7c7774b19ce6544112d9078d369653aa40c90c3
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-07 14:27:25 +0100

    #004 Generated LoginPageComponent

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,19 +2,22 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
 import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild([
-      {path: '', component: AdminLayoutComponent}
+      {path: '', component: AdminLayoutComponent, children:[
+        ]}
     ])
   ],
   exports:[
     RouterModule
   ],
   declarations: [
-    AdminLayoutComponent
+    AdminLayoutComponent,
+    LoginPageComponent
   ]
 })
 export class AdminModule{}
