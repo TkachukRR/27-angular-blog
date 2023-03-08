56b28cd39926c436247a7ab559ba190533379326
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-08 23:02:41 +0100

    #007 Added token and isAuthenticated() to auth.service

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -6,10 +6,16 @@ import {User} from "../../../shared/interfaces";
 export class AuthService{
   constructor(private http: HttpClient) {
   }
-
+  get token(): string {
+    return ''
+  }
   login(user: User) {
     this.http.post('', user)
   }
 
   logout(){}
+
+  isAuthenticated(): boolean {
+    return !!this.token
+  }
 }
