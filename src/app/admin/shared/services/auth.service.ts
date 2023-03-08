59ca31680ea2537ae68b9aacec666c4b984808c6
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-08 23:01:05 +0100

    #007 Added login() logout() to auth.service

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,8 +1,15 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
+import {User} from "../../../shared/interfaces";
 
 @Injectable()
 export class AuthService{
   constructor(private http: HttpClient) {
   }
+
+  login(user: User) {
+    this.http.post('', user)
+  }
+
+  logout(){}
 }
