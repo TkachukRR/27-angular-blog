335b32a6fc895a4cbf425d212ebf4f2174f11ae4
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-08 16:33:11 +0100

    #007 Added import to app.module and admin.module SharedModule

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,12 +8,14 @@ import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {AuthService} from "./shared/services/auth.service";
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    SharedModule,
     RouterModule.forChild([
       {path: '', component: AdminLayoutComponent, children:[
           {path: '', redirectTo: '/admin/login', pathMatch: "full"},
