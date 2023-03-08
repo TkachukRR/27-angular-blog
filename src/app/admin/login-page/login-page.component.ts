1c3b3d5858a1f9cc7339f22006f9c8b6370ef132
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-08 23:25:42 +0100

    #007 Added login() and subscribe to submit() in login.page

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -39,5 +39,10 @@ export class LoginPageComponent implements OnInit{
       email: this.form.value.email,
       password: this.form.value.password,
     }
+
+    this.auth.login(user).subscribe(() =>{
+      this.form.reset()
+      this.router.navigate(['/admin', 'dashboard'])
+    })
   }
 }
