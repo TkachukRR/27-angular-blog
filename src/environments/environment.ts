57e66a2c22afe8cb476e50680c6764b2861515df
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-09 13:22:01 +0100

    #008 Added apiKey and environment: type

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,6 @@
-export const environment = {
+import {Environment} from "./interface";
+
+export const environment: Environment = {
   production: false,
+  apiKey: 'AIzaSyApy9Ns7RIM9lj32VD7KnzOeVM8i6k4Inc',
 }
