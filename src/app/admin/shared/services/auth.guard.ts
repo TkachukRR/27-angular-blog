a549568cb15710a2a89d5cd233d39de66da4bdd6
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-10 12:15:59 +0100

    #008 Injected AuthService and Router to AuthGuard

--- src/app/admin/shared/services/auth.guard.ts
+++ src/app/admin/shared/services/auth.guard.ts
@@ -1,9 +1,15 @@
 import {Injectable} from "@angular/core";
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from "@angular/router";
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
 import {Observable} from "rxjs";
+import {AuthService} from "./auth.service";
 
 @Injectable()
 export class AuthGuard implements CanActivate{
+  constructor(
+    private auth: AuthService,
+    private router: Router
+    ) {
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
