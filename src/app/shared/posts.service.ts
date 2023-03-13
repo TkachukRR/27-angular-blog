c6f6ddf6fbdfebb80a3c75b925a7be79c7d65b5c
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-13 15:20:08 +0100

    #009 Added id to post

--- src/app/shared/posts.service.ts
+++ src/app/shared/posts.service.ts
@@ -1,8 +1,9 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {Post} from "./interfaces";
+import {FbCreateResponse, Post} from "./interfaces";
 import {environment} from "../../environments/environment";
+import {map} from "rxjs/operators";
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -11,5 +12,12 @@ export class PostsService {
 
   create(post: Post): Observable<Post> {
     return this.http.post<Post>(`${environment.fbDbUrl}/posts.json`, post)
+      .pipe(map((response: FbCreateResponse | any) => {
+        return {
+          ...post,
+          id: response.name,
+          date: new Date(post.date)
+        }
+      }))
   }
 }
