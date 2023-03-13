6a11bc82ec333909ace0eb2212d493655a3561f4
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-13 23:05:11 +0100

    #010 Added posability get posts in DashboardComponent

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PostsService} from "../../shared/posts.service";
+import {Post} from "../../shared/interfaces";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent {
+export class DashboardPageComponent implements OnInit, OnDestroy{
+  posts: Post[]
+  pSub: Subscription
+  constructor(private postsService: PostsService) {
+  }
 
+  ngOnInit() {
+    this.pSub = this.postsService.getAll().subscribe(posts => {
+      this.posts = posts
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.pSub) {
+      this.pSub.unsubscribe()
+    }
+  }
 }
