d48a276f59a7ed99b6070bf3967209f3d68c9923
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-13 21:39:45 +0100

    #009 Injected AuthService and Router to AuthInterceptor

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -1,9 +1,15 @@
 import {Injectable} from "@angular/core";
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import { Observable } from "rxjs";
+import {AuthService} from "../admin/shared/services/auth.service";
+import {Router} from "@angular/router";
 
 @Injectable()
 export  class AuthInterceptor implements HttpInterceptor{
+  constructor(
+    private authService: AuthService,
+    private router: Router) {
+  }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return undefined;
   }
