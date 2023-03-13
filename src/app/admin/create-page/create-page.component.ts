227a2c2b6bb2c52c1a494522483a65de8cfdc8b1
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-13 10:42:59 +0100

    #009 Created object form: FormGroup

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-create-page',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-page.component.scss']
 })
 export class CreatePageComponent {
-
+  form : FormGroup
 }
