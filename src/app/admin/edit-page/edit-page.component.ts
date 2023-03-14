75ea35e78b5f7728bff80e40f71ff5fc794f1e29
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-14 09:56:06 +0100

    #011 Implemented OnInit and injected ActivatedRoute and PostService

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {PostsService} from "../../shared/posts.service";
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.scss']
 })
-export class EditPageComponent {
+export class EditPageComponent implements OnInit{
+constructor(
+  private route: ActivatedRoute,
+  private postsService: PostsService
+) {
+}
 
+ngOnInit(){}
 }
