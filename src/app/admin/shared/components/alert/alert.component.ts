a3939e7d6de7d698f7b3aba2e7fcb3c9f8912701
Roman Tkachuk <tkachukrr@gmail.com>
2023-03-14 12:25:07 +0100

    #012 Added basic variables to AlertComponent

--- src/app/admin/shared/components/alert/alert.component.ts
+++ src/app/admin/shared/components/alert/alert.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-alert',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./alert.component.scss']
 })
 export class AlertComponent {
+  @Input() delay = 5000
+
+  public text: string
+  public type = 'success'
 
 }
